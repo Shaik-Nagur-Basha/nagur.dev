d23383c8ad319bcb889839da42b7f85e8baa2590
Shaik Nagur Basha <sknbasknba@gmail.com>
2026-01-21 16:50:52 +0530

    chore: update SPA routing in index.html and 404.html; modify App and main components for improved routing structure

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,12 +10,10 @@ import "./style.css";
 // import IframePage from "./pages/IframePage";
 
 function App() {
-  const basePath = import.meta.env.BASE_URL.replace(/\/$/, "");
-
   return (
     <ThemeProvider>
       <ScrollToTop />
-      <Routes basename={basePath}>
+      <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/projects" element={<ProjectsPage />} />
         {/* <Route path="/iframe" element={<IframePage />} /> */}
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/nagur.dev/",
+  base: "/nagur.dev",
   plugins: [react(), tailwindcss()],
 });
